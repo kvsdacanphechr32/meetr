3405779a6f5021d29c7eb58c3798791afa51f224
Johnny Richardson <johnny.c.rich@gmail.com>
2019-07-19 12:52:11 -0400

    Cleanup of case studies.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Ensure in root and run deploy after logging in
-ssh -i deploy_key node@"$1" "cd /srv/meetr/source && git pull origin master && bash deploy.sh qa"
+ssh -i deploy_key node@"$1" "cd /srv/meetr/source && git pull origin master && bash deploy.sh qa" -vvvv
