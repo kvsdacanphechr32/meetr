b15e0a3844cbd152be5e497ce21dc6fa48fe279c
Johnny Richardson <johnny.c.rich@gmail.com>
2019-08-06 13:41:01 -0400

    Handle edge case in Firefox where oauth login results in multiple redirects from auth0 to /callback. Added favicons.

--- client/src/app/utils/callback/callback.component.ts
+++ client/src/app/utils/callback/callback.component.ts
@@ -15,7 +15,11 @@ export class CallbackComponent implements OnInit {
   async ngOnInit() {
 
     if(this.router.url.indexOf('/oauth') > 0) {
-      this.handleOAuth();
+      if(window.location.hash.length < 1)
+        this.router.navigate(['/projects']);
+      else
+        this.handleOAuth();
+
       return;
     }
 
